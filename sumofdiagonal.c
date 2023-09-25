802d60a364f45dad72f9a1ba92ed71b8e903b5e9
Piyush Patil <136091080+Piyush-patil0031@users.noreply.github.com>
2023-09-25 11:40:56 +0530

    Update sumofdiagonal.c

--- sumofdiagonal.c
+++ sumofdiagonal.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int r, c, sum, b;
-	r = c = sum = b = 0;
+    int r, c, sum;
+	r = c = sum = 0;
 	
 	//taking input of rows and columns for both matrices
 	printf("Enter the number of rows and columns for matrix  ");
@@ -19,17 +19,20 @@ int main() {
 		}
 		
 		//calculating sum of diagonal elements
-		 for (i = 0; i < r; ++i) {
+		for (i = 0; i < r; ++i) {
             
-            // calculating the main diagonal sum
-            sum = sum + a[i][i];
-            
-            // calculating the off diagonal sum
-            b = b + a[i][r - i - 1];
-        }
-	    
-	    
-		printf("sum of diagonal elements is %d\n", sum);
-		printf("sum of off diagonal elements is %d\n", b);
-    return 0;
+            		// calculating the main diagonal sum
+            		sum = sum + a[i][i];
+        	}
+	
+		       // calculating the off diagonal sum
+	    	for(i=0; i<r; i++){
+			for(j=0; j<c; j++){
+			    if(i + j == r-1){
+			         sum = sum + a[i][i];
+			    }
+			}
+		}
+printf("sum of diagonal elements is %d\n", sum);
+return 0;
 }
