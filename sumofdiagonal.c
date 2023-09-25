7998dc66afe24aadfd85af2e2b4f249337fc1ec3
Piyush Patil <136091080+Piyush-patil0031@users.noreply.github.com>
2023-09-26 01:07:07 +0530

    Update sumofdiagonal.c

--- sumofdiagonal.c
+++ sumofdiagonal.c
@@ -2,36 +2,43 @@
 
 int main() {
     int r, c, sum;
-  r = c = sum = 0;
-  
-  //taking input of rows and columns for both matrices
-  printf("Enter the number of rows and columns for matrix  ");
-  scanf("%d %d", &r, &c);
-  
-  int a[r][c], i, j;
+    r = c = sum = 0;
     
+    //taking input of rows and columns for both matrices
+    printf("Enter the number of rows and columns for matrix  ");
+    scanf("%d %d", &r, &c);
+    
+    int a[r][c], i, j;
+        
     //taking input for matrix a
     printf("Enter the values for matrix a\n");
     for(i=0; i<r; i++){
-      for(j=0; j<c; j++){
-        scanf("%d", &a[i][j]);
-      }
+        for(j=0; j<c; j++){
+            scanf("%d", &a[i][j]);
+        }
     }
-    
+        
     //calculating sum of diagonal elements
     for (i = 0; i < r; ++i) {
-            
-                // calculating the main diagonal sum
-                sum = sum + a[i][i];
                 
+            // calculating the main diagonal sum
+            sum = sum + a[i][i];
+                    
             for(j=0; j<c; j++){     
                 // calculating the off diagonal sum
                 if(i + j == r-1){
-                     sum = sum + a[i][i];
+                    sum = sum + a[i][i];
                 }
             }
-     }
-  
-printf("sum of diagonal elements is %d\n", sum);
-return 0;
+
+            for(j=0; j<c; j++){     
+                // subtracting middle elelment which counted 2 times
+                if(i + j == r-1 && i == j){
+                    sum = sum - a[i][i];
+                }
+            }
+    }
+    
+    printf("sum of diagonal elements is %d\n", sum);
+    return 0;
 }
