dd9d025373f7757def2a8a556127e9f8b49e9691
Piyush Patil <136091080+Piyush-patil0031@users.noreply.github.com>
2023-10-10 19:11:56 +0530

    Update primefunction.c

--- primefunction.c
+++ primefunction.c
@@ -1,27 +1,18 @@
-
 #include<stdio.h>
 #include<stdbool.h>
-  
-bool isPrime(int n)
-{
+bool isPrime(int n){
     if(n==0 || n==1){
         return false;
       }
-   for(int i=2; i<n ; i++)
-   {
-      if(n%i==0)
-      {
+   for(int i=2; i<n ; i++){
+      if(n%i==0){
         return false;      
       }
    }
    return true;
-
 }
-
-int main()
-{
-  int N,i;
-  i=0;
+int main(){
+  int N,i=0;
   printf("Enter the num= ");
   scanf("%d", &N);
   for(i=0;i<N;i++){
