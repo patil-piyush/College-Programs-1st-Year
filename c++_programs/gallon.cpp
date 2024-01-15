5f37285de98131fc90918ad68ad2c27c7c40b03e
Piyush Patil <136091080+Piyush-patil0031@users.noreply.github.com>
2024-01-15 21:58:30 +0530

    Create gallon.cpp
    it converts amount of gallons into cubic feet

---
+++ c++_programs/gallon.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float gallon;
+    double cfeet;
+    cout<<"enter the gallons: ";
+    cin>>gallon;
+    cfeet=(gallon/7.481);
+    cout<<cfeet;
+    return 0;
+}
