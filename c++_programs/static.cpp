75fb0393c3b30bb0970b887d28575e8c8b2fed6e
Piyush <piyushpatil0031@gmail.com>
2024-01-30 14:00:07 +0530

    static variable added

---
+++ c++_programs/static.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+class a{
+    int x;
+    static int y;
+    public:
+    void read();
+    void display();
+    a();
+};
+
+void a::read()
+{
+    cout<<"enter the non-static variable : ";
+    cin>>x;
+    cout<<"enter the static variable : ";
+    cin>>y;
+}
+void a::display()
+{
+    cout<<"the non-static variable is : "<<x<<endl;
+    cout<<"the static variable is : "<<y<<endl;
+}
+a::a()
+{
+    x=0;
+    y=0;
+}
+int main()
+{
+    a obj1;
+    a obj2;
+    obj1.read();
+    obj1.display();
+    obj2.read();
+    obj2.display();
+    return 0;
+}
+
