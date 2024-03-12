35e33050d71ab7c778720b1a651bc457b2d0cbb9
Piyush <piyushpatil0031@gmail.com>
2024-03-12 16:39:30 +0530

    started a new PYTHON course of code with harry and added my first program that is a simple calculator which is performing basic arithmetic on two numbers

---
+++ python_programs/CWH_PYTHON_Course/calculator.py
@@ -0,0 +1,10 @@
+num1 = 10
+num2 = 20
+ans1 = num1 + num2
+print("Addition of",num1, "and",num2, "is :",ans1)
+ans2 = num1 - num2
+print("Subtraction of",num1, "and",num2, "is :",ans2)
+ans3 = num1 * num2
+print("Multiplication of",num1, "and",num2, "is :",ans3)
+ans4 = num1 / num2
+print("Division of",num1, "and",num2, "is :",ans4)
