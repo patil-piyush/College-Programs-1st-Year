77978d1d822704e16e27aaeea3338d1dffcd7552
Piyush <piyushpatil0031@gmail.com>
2024-03-26 19:34:03 +0530

    Updated template.cpp

--- c++_programs/template/template.cpp
+++ c++_programs/template/template.cpp
@@ -1,18 +1,32 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
 
-class student{
-    
-    public:
-    template<class T>
-    void totalmarks(T a, T b){
-        T res = a + b;
-        cout<<"Total marks : "<<res<<endl;
+template <class T>
+class Student
+{
+public:
+    T mark1;
+    T mark2;
+
+    Student(T m1, T m2)
+    {
+        this->mark1 = m1;
+        this->mark2 = m2;
+    }
+
+    T calculateTotal()
+    {
+        return mark1 + mark2;
     }
 };
-int main(){
-    student s1, s2;
-    s1.totalmarks(20, 20);
-    s2.totalmarks(22.22, 22.22);
+
+int main()
+{
+
+    Student<int> student1(11, 99);
+    std::cout << "Total marks of student 1 (integer marks): " << student1.calculateTotal() << std::endl;
+
+    Student<float> student2(22.2, 44.4);
+    std::cout << "Total marks of student 2 (floating point marks): " << student2.calculateTotal() << std::endl;
+
     return 0;
 }
