a104925f28532f1129b073d37f7f456bf71935b7
Piyush Patil <piyushpatil0031@gmail.com>
2024-08-09 00:25:01 +0530

    student database is half done

---
+++ 2nd Year/DSA/Student_Database.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class Student{
+    private:
+    int roll;
+    string name;
+    float sgpa;
+
+    public:
+    void read();
+    void display();
+    friend void insertionSort();
+};
+
+void Student::read(){
+    
+}
+
+int main(){
+    
+    return 0;
+}
