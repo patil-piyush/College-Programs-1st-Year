d953f88647459d43cac0e0c989e5c287f94d255a
Piyush Patil <136091080+patil-piyush@users.noreply.github.com>
2024-08-28 10:17:55 +0530

    Create shape.py
    file contains the shape function of numpy

---
+++ 2nd Year/Sem 3/DE & VL/Numpy Operations/shape.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+#shape attribute returns the tuple of which each element shows the number of elements in corresponding dimentions
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+
+print(arr.shape)
+
+#--------------------------------------------------------------------------------------------
+
+"""
+import numpy as np
+
+#creating a array of dimention 5 by passing an argument ndim
+arr = np.array([1, 2, 3, 4], ndmin=5)
+
+#checking its shape
+print(arr)
+print(arr.shape)
+"""
