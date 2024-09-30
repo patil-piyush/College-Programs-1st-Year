6485e6a58130bc4984741cbda2c73d61ac852878
Piyush Patil <piyushpatil0031@gmail.com>
2024-09-30 10:37:22 +0530

    added the code for infix to postfix

--- 2nd Year/Sem 3/DSA/Stack/InfixToPostfix.cpp
+++ 2nd Year/Sem 3/DSA/Stack/InfixToPostfix.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 class stack
 {
 public:
-    char arr[100];
+    char inf[100];
     int top;
     stack()
     {
@@ -15,7 +16,6 @@ public:
     {
         if (top == -1)
         {
-            cout << "stack is empty\n";
             return 1;
         }
         return 0;
@@ -23,7 +23,7 @@ public:
 
     char pop()
     {
-        char c = arr[top];
+        char c = inf[top];
         top--;
         return c;
     }
@@ -31,69 +31,113 @@ public:
     void push(char a)
     {
         top++;
-        arr[top] = a;
+        inf[top] = a;
     }
 
     char peek()
     {
-        return arr[top];
+        return inf[top];
     }
 
     void read()
     {
-        cout << "Enter the expression: ";
-        for (int i = 0; i != '\n'; i++)
-        {
-            cin >> arr[++top];
-        }
+        cout << "Enter the Infix expression: ";
+        cin >> inf;
     }
 
     void display()
     {
-        cout << "The postfix expression is: ";
         if (isEmpty())
         {
-            cout<<"stack is empty"<<endl;
+            return;
         }
         else
         {
             while (top != -1)
             {
-                cout << arr[top--]<<" ";
+                cout << inf[top--] << " ";
             }
         }
     }
 
-    int precedance(char a){
-        if(a == '*' || a == '/'){
-            return 2;
-        }
-        else if(a == '+' || a == '-'){
+    // function for checking the character is a operator or not
+    int isOperator(char a)
+    {
+        if (a == '+' || a == '-' || a == '*' || a == '/' || a == '^')
+        {
             return 1;
         }
-        else if(a == '('){
+        return 0;
+    }
+
+    // function for checking precedance of operators
+    int precedance(char a)
+    {
+        if (a == '+' || a == '-')
+        {
+            return 1;
+        }
+        else if (a == '*' || a == '/')
+        {
+            return 2;
+        }
+        else if (a == '^')
+        {
             return 3;
         }
-        else if(a == ')'){
-
-        }
-        else{
+        else
+        {
             return 0;
         }
     }
-    char[] Postfix(){
-        newarr = new char[100];
-        postarr = new char[100];
-        for(int i=0; isEmpty == -1; i++){
-            if()
+
+    void Postfix()
+    {
+        int j = 0;
+        char post[100];
+        for (int i = 0; i < strlen(inf); i++)
+        {
+            int x = inf[i];
+
+            if (inf[i] >= 'a' && inf[i] <= 'z' || inf[i] >= 'A' && inf[i] <= 'Z')
+            {
+                post[j++] = x;
+            }
+            else if (isOperator(x))
+            {
+                while (precedance(peek()) >= precedance(x))
+                {
+                    post[j++] = pop();
+                }
+                push(x);
+            }
+            else if (x == '(')
+            {
+                push(x);
+            }
+
+            else if (x == ')')
+            {
+                while (peek() != '('){
+                    post[j++] = pop();
+                }
+                pop();
+            }
         }
+        while (!isEmpty())
+        {
+            post[j++] = pop();
+        }
+        post[j] = '\0';
+        cout<<"POSTFIX Expression: "<<post;
     }
 };
 
-
-
 int main()
 {
-    
+    stack p;
+    p.read();
+    p.display();
+    p.Postfix();
     return 0;
 }
